f3755e8525befda2c563fdae1ab1d146896704d8
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2021-09-15 21:30:11 +0530

    Topping tested succussfully

--- client/sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
+++ client/sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
@@ -14,3 +14,17 @@ test('display image for each scoop option from server', async() => {
     const altText = scoopImages.map((element) => element.alt)
     expect(altText).toEqual(['Chocolate scoop' , 'Vanilla scoop'])
 })
+
+test('display image for each toppings option from server', async() => {
+    render(<Options optionType = "toppings" />)
+
+    // find image
+    const toppingImages = await screen.findAllByRole('img' , {name : /topping$/i})
+    expect(toppingImages).toHaveLength(3)
+
+    // confirm alt test of images
+
+    const altText = toppingImages.map((element) => element.alt)
+    expect(altText).toEqual(['Cherries topping' , 'M&Ms topping' , 'Hot fudge topping'])
+})
+
