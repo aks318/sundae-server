85b51af3e7537eaa28aa435bc2476ad19b7ee394
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2021-09-22 13:24:41 +0530

    toppping option tested

--- client/sundaes-on-demand/src/pages/entry/tests/totalUpdates.test.jsx
+++ client/sundaes-on-demand/src/pages/entry/tests/totalUpdates.test.jsx
@@ -3,7 +3,7 @@ import userEvent from "@testing-library/user-event"
 import { OrderDetailsProvider } from "../../../contexts/OrderDetails";
 import Options from "../Options";
 
-test.only('updates scoop subtotal when scoop change', async () => {
+test('updates scoop subtotal when scoop change', async () => {
     render(<Options optionType = "scoops" /> , {wrapper : OrderDetailsProvider})
 
     // make sure total starts out $0.0
@@ -25,3 +25,28 @@ test.only('updates scoop subtotal when scoop change', async () => {
     expect(scoopsSubtotal).toHaveTextContent('6.00')
 
 })
+
+test('update toppings subtotal when toppings change', async () => {
+    // render parent component
+    render(<Options optionType="toppings" /> , {wrapper : OrderDetailsProvider});
+  
+    // make sure total starts out at $0.00
+    const toppingsTotal = screen.getByText('Toppings total: $', { exact: false });
+    expect(toppingsTotal).toHaveTextContent('0.00');
+  
+    // add cherries and check subtotal
+    const cherriesCheckbox = await screen.findByRole('checkbox', {
+      name: 'Cherries',
+    });
+    userEvent.click(cherriesCheckbox);
+    expect(toppingsTotal).toHaveTextContent('1.50');
+  
+    // add hot fudge and check subtotal
+    const hotFudgeCheckbox = screen.getByRole('checkbox', { name: 'Hot fudge' });
+    userEvent.click(hotFudgeCheckbox);
+    expect(toppingsTotal).toHaveTextContent('3.00');
+  
+    // remove hot fudge and check subtotal
+    userEvent.click(hotFudgeCheckbox);
+    expect(toppingsTotal).toHaveTextContent('1.50');
+  });
